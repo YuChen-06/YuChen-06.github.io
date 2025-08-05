a14904fa18c4fe987bc2e54827bc2a71f5809dd0
Yuchen <2532962458@qq.com>
2025-08-05 14:33:34 +0800

    update test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  base: '/',
+  build: {
+    outDir: 'docs', // 将构建输出放到 /docs 以支持 GitHub Pages
+  },
+  site: 'https://www.yuchen06.xyz', // 用你的自定义域名
+});
